9755a85ede2cc9b23c7b0f4cb1abbfad91f29ed1
Jonathan <30727873+ea1het@users.noreply.github.com>
2020-03-23 14:12:05 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=locally-disabled, multiple-statements
+# pylint: disable=fixme, line-too-long, invalid-name
+# pylint: disable=W0703
+
+""" Coronavirus-Extractor: An effort to track potential fake news sources """
+
+__author__ = 'EA1HET'
+__date__ = "23/03/2020"
+
+import re
+import date
+import datetime
+
+DATEFORMAT = datetime.date.today().isoformat()
+print(DATEFORMAT)
+
